955f51679674b3a26f71ca4f59bc0d20f548f377
Carlos Eduardo SIlva Nascimento Gomes <2151424@my.ipleiria.pt>
2018-01-13 21:10:07 +0000

    minor

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,12 +3,9 @@
 Dear {{$user->name}},
 click the button bellow to ative your account
 
-<<<<<<< HEAD
 <!-- URL::asset('http://proj_dad.teste/api/activateAccount/'.$user->id)-->
 @component('mail::button', ['url' => 'http://proj_dad.teste'])
-=======
-@component('mail::button', ['url' => 'http://dadproj.dad'])
->>>>>>> 013d4fcf48a035298c6f245c9fe4845be1b4adda
+
 Activate
 @endcomponent
 
